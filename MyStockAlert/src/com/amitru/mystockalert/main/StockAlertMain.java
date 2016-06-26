5073b943dcfe55533b7ce9073b89b39b88a42acc
AmitRuwali <amit.ruwali@gmail.com>
2016-06-26 23:27:26 +0900

    AddedTimer

--- MyStockAlert/src/com/amitru/mystockalert/main/StockAlertMain.java
+++ MyStockAlert/src/com/amitru/mystockalert/main/StockAlertMain.java
@@ -1,5 +1,10 @@
 package com.amitru.mystockalert.main;
 
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import com.amitru.mystockalert.impl.StockAlertImpl;
 
 public class StockAlertMain {
@@ -10,12 +15,35 @@ public class StockAlertMain {
 	
 	public static void main(String[] args) throws Exception {
 		
+		
+		ScheduledExecutorService executor =
+			    Executors.newSingleThreadScheduledExecutor();
+
+			Runnable periodicTask = new Runnable() {
+			    public void run() {
+			        // Invoke method(s) to do the work
+			        try {
+			        	doTasks();
+			        } catch(Exception e) {
+			        	e.printStackTrace();
+			        }
+			    }
+			};
+
+			executor.scheduleAtFixedRate(periodicTask, 0, 10, TimeUnit.MINUTES);
+		
+		
+		
+	}
+	
+	private static void doTasks() throws Exception {
+		
 		//STEP 1: Read DATA from source, DB,XLS or any VO
 		StockAlertImpl.getInstance().readDataFromSource();
-		
+				
 		//STEP 2: CALL WS AND FETCH SHARE DETAILS, CALL THIS METHOD ON EVERY 5 MINS OR SO
 		StockAlertImpl.getInstance().fetchWatchListShareDetails();
-		
+				
 		//STEP3: TRIGGER EMAIL FOR SELCTED SCRIPTS found at STEP-2
 		StockAlertImpl.getInstance().triggerEmailForWatchList();
 		
